6ba32869a245498550869392de638dc1357f66b9
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2023-11-29 18:32:01 +0700

    Cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,8 +69,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation ("androidx.compose.material:material:1.5.4")
-    implementation ("androidx.compose.material:material-icons-core:1.5.4")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
