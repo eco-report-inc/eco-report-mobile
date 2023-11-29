d2f2d30e8253cdd373617fbdb50d62f9fac3929c
RanggaNf <142804080+RanggaNf@users.noreply.github.com>
2023-11-29 19:19:48 +0700

    Navigasi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,9 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation ("androidx.compose.material:material:1.5.4")
+    implementation ("androidx.compose.material:material-icons-core:1.5.4")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha09")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
