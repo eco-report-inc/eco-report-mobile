d25aea194a49633d426dca8380e8b26645412fab
RanggaNf <142804080+RanggaNf@users.noreply.github.com>
2023-12-02 13:31:55 +0700

    Auth & Navigation logic

--- app/src/main/java/com/capstone/ecoreport/ui/theme/screen/LoginScreen.kt
+++ app/src/main/java/com/capstone/ecoreport/ui/theme/screen/LoginScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
