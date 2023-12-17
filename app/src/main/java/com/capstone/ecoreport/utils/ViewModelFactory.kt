17693dd82d41da76b2bad32c7709653669e36bd6
RanggaNf <142804080+RanggaNf@users.noreply.github.com>
2023-12-17 10:51:41 +0700

    Busiines

--- app/src/main/java/com/capstone/ecoreport/utils/ViewModelFactory.kt
+++ app/src/main/java/com/capstone/ecoreport/utils/ViewModelFactory.kt
@@ -1,6 +1,6 @@
 package com.capstone.ecoreport.ui.viewmodel
 
-/*import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.ecoreport.data.Report.ReportRepository
 import com.capstone.ecoreport.data.auth.AuthRepository
@@ -28,4 +28,3 @@ class ModelFactory(
         }
     }
 }
- */
