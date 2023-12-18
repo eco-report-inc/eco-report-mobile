04e0c026195a306797f7d9127ac562015393f0fe
RanggaNf <142804080+RanggaNf@users.noreply.github.com>
2023-12-18 17:27:17 +0700

    Navigation

--- app/src/main/java/com/capstone/ecoreport/navigation/Screen.kt
+++ app/src/main/java/com/capstone/ecoreport/navigation/Screen.kt
@@ -1,13 +1,13 @@
 package com.capstone.ecoreport.navigation
 
 sealed class Screen(val route: String) {
+    object Login: Screen("login")
+    object Register: Screen("register")
     object Home : Screen("home")
     object Maps : Screen("maps")
     object Profile : Screen("profile")
     object Detail : Screen("home/{dummyId}") {
         fun createRoute(dummyId: Int) = "home/$dummyId"
     }
-    object Login : Screen("login")
-    object Register : Screen("register")
+    object EditProfile : Screen("editProfile")
 }
-
